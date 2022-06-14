5ca083dc6f91e65e6e45c41a19ee1edec000dacb
bingyuanng <bingyuanng@gmail.com>
2022-06-14 18:04:38 +0800

    Improvements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Index from "./components/Search/Index"
 
+import 'antd/dist/antd.css'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{marginLeft: 'auto', marginRight: 'auto', width: '800px'}}>
       <Index />
     </div>
   );
